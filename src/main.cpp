11afef1e56341797ff9c2793f4a93f91c7e5cc67
William <wdeeken7@yahoo.com>
2023-10-01 19:17:22 -0500

    Initial code commit, using example as a base.

---
+++ src/main.cpp
@@ -0,0 +1,108 @@
+/* Heltec Automation send communication test example
+ *
+ * Function:
+ * 1. Send data from a CubeCell device over hardware 
+ * 
+ * 
+ * this project also realess in GitHub:
+ * https://github.com/HelTecAutomation/ASR650x-Arduino
+ * */
+
+#include "LoRaWan_APP.h"
+#include "Arduino.h"
+
+/*
+ * set LoraWan_RGB to 1,the RGB active in loraWan
+ * RGB red means sending;
+ * RGB green means received done;
+ */
+#ifndef LoraWan_RGB
+#define LoraWan_RGB 0
+#endif
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 30 // Define the payload size here
+
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+
+float txNumber;
+bool lora_idle=true;
+void OnTxDone(void);
+void OnTxTimeout();
+void setup() {
+    Serial.begin(115200);
+
+    txNumber=0;
+
+    RadioEvents.TxDone = OnTxDone;
+    RadioEvents.TxTimeout = OnTxTimeout;
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                                   LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                                   LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                   true, 0, 0, LORA_IQ_INVERSION_ON, 3000 ); 
+
+                                   
+    // setup motion input.
+  pinMode(GPIO1, INPUT);
+}
+
+
+
+void loop()
+{
+  if(lora_idle == true)
+  {
+    // First, check if motion is detected.
+    if(digitalRead(GPIO1) == 1){
+        txNumber += 0.01;
+        sprintf(txpacket,"Hello world number %0.2f",txNumber);  //start a package
+        Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
+        turnOnRGB(COLOR_SEND,0); //change rgb color
+        Radio.Send( (uint8_t *)txpacket, strlen(txpacket) ); //send the package out 
+        lora_idle = false;
+    }
+    delay(1000);
+    
+  }
+}
+
+
+void OnTxDone( void )
+{
+  turnOffRGB();
+  Serial.println("TX done......");
+  lora_idle = true;
+}
+
+void OnTxTimeout( void )
+{
+  turnOffRGB();
+  Radio.Sleep( );
+  Serial.println("TX Timeout......");
+  lora_idle = true;
+}
