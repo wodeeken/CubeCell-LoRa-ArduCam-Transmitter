7731a14abf8f091b1dc0c6348ccbfbae50ced22a
wdeeken7@yahoo.com <wdeeken7@yahoo.com>
2024-08-18 17:40:54 -0500

    Implemented data transfer logic.

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -2,10 +2,26 @@
 struct Constants{
     static constexpr char* FromRec_Ping_Camera_Command = "<PING_CAMERA>";
     static constexpr char* ToRec_Ping_Camera_Response = "<PING_CAMERA_RESPONSE>";
+    static constexpr char* FromRec_Camera_Image_Request_Command = "<CAPTURE_CAMERA_REQUEST>";
+    static constexpr char* ToRec_Camera_Image_Request_Response = "<CAPTURE_CAMERA_RESPONSE {PACKETS}>";
+    static constexpr char* FromRec_Data_Transfer_Command = "^<DATA_TRANSFER_REQUEST {%d+}>$";
+    static constexpr char* ToRec_Data_Transfer_Response_Header = "<DATA_TRANSFER_RESPONSE {PACKET_NUM}>";
+    static constexpr char* ToRec_Data_Transfer_Response_Footer = "</DATA_TRANSFER_RESPONSE>";
+    static constexpr char* ToRec_Camera_Malfunction = "<CAMERA_ERROR>";
     static const int RecTimeout = 3000;
+    static const int PacketSize = 80;
+    // Time to wait for confirmation to begin data transfer, or the time between a packet send and t
+    //the request to send the next one.
+    static const int DataTransferWaitTimeout = 5000; 
     enum TransmitterState{
         Wait, // Doing nothing, waiting to enter into Rx mode.
         RxMode, // In Rx mode, waiting to either recieve something over the radio, or timeout.
         CameraPingRequest, // Camera ping request over radio, respond back.
+        CameraCaptureRequest, // Camera capture request over radio, trigger camera.
+        CameraCaptureWait, // Camera is currently working. Wait until image is finished. 
+        CameraCaptureComplete, // Camera is finished working, or failed. Sending
+        DataTransferRequestWait, // Wait until receiving OK to begin data transfer, or until timeout.
+        DataTransferPacketSend, // Sending a packet.
+        DataTransferPacketWait // Wait until confirmation from receiver to send another, or until timeout. 
     };
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "LoRaWan_APP.h"
 #include "Arduino.h"
 #include "Constants.cpp"
+#include <Regexp.h>
 // Lora definitions.
 #ifndef LoraWan_RGB
 #define LoraWan_RGB 0
@@ -34,6 +35,7 @@ bool RxTimedout = false;
 bool lora_idle = true;
 char txpacket[BUFFER_SIZE];
 char rxpacket[BUFFER_SIZE];
+int DataRequestPacket = 0;
 //char* rxpacket_trunc;
 static RadioEvents_t RadioEvents;
 void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr );
@@ -42,6 +44,8 @@ void OnTxTimeout();
 void OnRxTimeout();
 Constants::TransmitterState CurrentTransmitterState = Constants::Wait;
 
+// MOCK Camera data.
+char* MockCameraData = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200";
 void setup() {
     // Setup serial.
     Serial.begin(38400);
@@ -89,12 +93,36 @@ void loop()
           Serial.println(rxpacket);
           // Convert to string for ease of comparison.
           String str = String(rxpacket);
-          str.trim();
+          
+          
+          // Use regex for data transfer request.
+          MatchState ms;
+          ms.Target(rxpacket);
+
+          char drResult = ms.Match(Constants::FromRec_Data_Transfer_Command);
+          Serial.println("result from match: ");
+          Serial.println(drResult );
           //Serial.println("1:" + str + "|");
           //Serial.println("1:" + String(Constants::FromRec_Ping_Camera_Command) + "|");
-          if(str.compareTo(String(Constants::FromRec_Ping_Camera_Command)) == 0){
+          if(str == ""){
+            CurrentTransmitterState = Constants::Wait;
+          }else if(str.compareTo(String(Constants::FromRec_Ping_Camera_Command)) == 0){
             Serial.println("Just got a ping request.");
             CurrentTransmitterState = Constants::CameraPingRequest;
+          }else if(str.compareTo(String(Constants::FromRec_Camera_Image_Request_Command)) == 0){
+            Serial.println("Just got a camera request.");
+            CurrentTransmitterState = Constants::CameraCaptureRequest;
+          }else if(drResult == REGEXP_MATCHED){
+            Serial.println("Just got a data request.");
+            char packetNumResult = ms.Match("%d+");
+            if(packetNumResult == REGEXP_MATCHED){
+              DataRequestPacket = str.substring(ms.MatchStart, ms.MatchStart + ms.MatchLength).toInt();
+              CurrentTransmitterState = Constants::DataTransferPacketSend;
+            }else{
+              Serial.println("Data request, could not find packet number.");
+              CurrentTransmitterState = Constants::Wait;
+            }
+            
           }else{
             Serial.println("Unrecognized Request.");
             CurrentTransmitterState = Constants::Wait;
@@ -113,10 +141,56 @@ void loop()
         Serial.println(txpacket);
         Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
         CurrentTransmitterState = Constants::Wait;
-      }else
+      }else;
         // Stay in state until lora isn't busy.
       
       break;
+      case Constants::CameraCaptureRequest:
+        // TO DO: implement camera logic. Use mock data for now.
+        CurrentTransmitterState = Constants::CameraCaptureWait;
+      break;
+      case Constants::CameraCaptureWait:
+      // Do nothing while camera works. 
+        CurrentTransmitterState = Constants::CameraCaptureComplete;
+      break;
+      case Constants::CameraCaptureComplete:
+        // Send back response with num of packets.
+        Serial.println("We inside the camera capture complete case..");
+        // Send data back, and go back to waiting.
+        if(lora_idle){
+        
+          lora_idle = false;
+          String response = String(Constants::ToRec_Camera_Image_Request_Response);
+          response.replace("PACKETS", String((strlen(MockCameraData) / Constants::PacketSize) + 1));
+          sprintf(txpacket, response.c_str());
+          Serial.print("About to send a packet!");
+          Serial.println(txpacket);
+          Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
+          CurrentTransmitterState = Constants::Wait;
+        }else;
+          // Stay in state until lora isn't busy.
+      break;
+      case Constants::DataTransferPacketSend:
+        Serial.println("We inside the data transfer case..");
+        // Send data back, and go back to waiting.
+        if(lora_idle){
+        
+          lora_idle = false;
+          String response = String(Constants::ToRec_Data_Transfer_Response_Header);
+          response.replace("PACKET_NUM", String(DataRequestPacket));
+          char currentPacketData[Constants::PacketSize];
+          memcpy(currentPacketData, &MockCameraData[(DataRequestPacket * Constants::PacketSize)], Constants::PacketSize);
+          response += String(currentPacketData) + String(Constants::ToRec_Data_Transfer_Response_Footer);
+          
+          sprintf(txpacket, response.c_str());
+          Serial.print("About to send a packet!");
+          Serial.println(txpacket);
+          Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
+          CurrentTransmitterState = Constants::Wait;
+        }else;
+          // Stay in state until lora isn't busy.
+      break;
+
   }
   
 }
